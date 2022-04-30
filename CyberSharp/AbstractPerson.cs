7c596673ccb0d13015e4285039cf4fa5e653f656
Marek Laššák <73712209+mlx7@users.noreply.github.com>
2022-04-30 19:03:24 +0200

    Update AbstractPerson.cs

--- CyberSharp/AbstractPerson.cs
+++ CyberSharp/AbstractPerson.cs
@@ -6,14 +6,9 @@ namespace CyberSharp
     {
         public string Name { get; init; } = Generator.GetRandomName();
         public string IpAdress { get; init; } = Generator.GetRandomIp();
-        //public string BtcValletAdress { get; init; }
-        //public string Password { get; init; }
         public BitcoinVallet BtcVallet { get; init; }
         public int HackCounter { get; set; } = 0;
         public int CurrentSuccess { get; set; } = 0;
-
-
-        //public AbstractPerson() { }
 
         public override string ToString() => $"Name: {Name}, IP: {IpAdress}";
 
