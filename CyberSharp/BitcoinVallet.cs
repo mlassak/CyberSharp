5be83d0f8aea170dc987f12f62a86e66f6f99066
Marek Laššák <73712209+mlx7@users.noreply.github.com>
2022-04-30 19:02:35 +0200

    Update BitcoinVallet.cs

--- CyberSharp/BitcoinVallet.cs
+++ CyberSharp/BitcoinVallet.cs
@@ -2,51 +2,51 @@
 
 namespace CyberSharp
 {
-
 	public class BitcoinVallet
 	{
 		public decimal Balance { get; set; }
-        public string Adress { get; init; }
-        public string Password { get; set; }
+        	public string Adress { get; init; }
+        	public string Password { get; set; }
 
 
-        public BitcoinVallet(decimal initialAmount, string adress, string password)
-        {
-            if (initialAmount < 0)
-            {
-                throw new ArgumentException("Cannot deposit a negative amount of BTC");
-            }
+        	public BitcoinVallet(decimal initialAmount, string adress, string password)
+        	{
+            		if (initialAmount < 0)
+            		{
+                		throw new ArgumentException("Cannot deposit a negative amount of BTC");
+            		}
 
-            Balance = initialAmount;
-            Adress = adress;
-            Password = password;
-        }
+            		Balance = initialAmount;
+            		Adress = adress;
+            		Password = password;
+        	}
 
 
-        public void Deposit(decimal amount)
-        {
-            if (amount < 0)
-            {
-                throw new ArgumentException("Cannot deposit a negative amount of BTC");
-            }
-
-            Balance = Balance + amount;
-        }
+        	public void Deposit(decimal amount)
+        	{
+        	    if (amount < 0)
+        	    {
+        	        throw new ArgumentException("Cannot deposit a negative amount of BTC");
+        	    }
+	
+        	    Balance = Balance + amount;
+        	}
 
 
 		public void Withdraw(decimal amount)
-        {
-            if (amount < 0)            
-            {
-                throw new ArgumentException("Cannot withdraw a negative amount of BTC");
-            }
-			if (Balance - amount < 0)
-            {
-                throw new ArgumentException("Cannot withdraw more than the vallet contains");
-            }
-
-            Balance -= amount;
-        }
+        	{
+        	    if (amount < 0)            
+        	    {
+        	        throw new ArgumentException("Cannot withdraw a negative amount of BTC");
+        	    }
+			
+		    if (Balance - amount < 0)
+        	    {
+        	        throw new ArgumentException("Cannot withdraw more than the vallet contains");
+        	    }
+	
+        	    Balance -= amount;
+        	}
 	}
 
 }
